f38316982cd3a51f497986318e7d3f6a3d42f933
FernandoSilvera <fernando.silvera02@gmail.com>
2022-05-25 16:47:01 +0200

    Project bootstrap, snack1 completed.

---
+++ snack1/snack1script.js
@@ -0,0 +1,10 @@
+// Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
+
+let nums = [];
+let sum = 0;
+
+do {
+  const userNum = parseInt(prompt("Inserisci un numero"));
+
+  sum += userNum;
+} while (sum < 50);
